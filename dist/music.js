ccaf00b3c641f24629e8a83eb58732bfe3911cc6
mendax92 <1034802111@qq.com>
2019-07-30 11:37:25 +0800

    Site updated: 2019-07-30 11:37:22

---
+++ dist/music.js
@@ -0,0 +1,37 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "你好陌生人",
+        artist: '',
+        url: 'https://uploader.shimo.im/f/w09PQZEFVsksW5Ja.mp3',
+        cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
+      },
+      {
+        name: '最熟悉的陌生人',
+        artist: '',
+        url: 'https://uploader.shimo.im/f/jyIMyNOkyKcL3PII.mp3',
+        cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
+      },
+      {
+        name: '十年',
+        artist: '',
+        url: 'https://uploader.shimo.im/f/rTg6EOdpRPw7rHKp.mp3',
+        cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
+      },
+      {
+        name: '再回首',
+        artist: '',
+        url: 'https://uploader.shimo.im/f/77qpMJRv6zQPbJWC.mp3',
+        cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
+      },
+      {
+        name: '一无所有',
+        artist: '',
+        url: 'https://uploader.shimo.im/f/l8F9G0DKBC8XUIC6.mp3',
+        cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
+      }
+    ]
+});
