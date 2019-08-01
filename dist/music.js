5eeba6bef1c7361e49f523646879f03d390ac7be
mendax92 <1034802111@qq.com>
2019-08-01 15:37:08 +0800

    Site updated: 2019-08-01 15:37:05

--- dist/music.js
+++ dist/music.js
@@ -6,31 +6,31 @@ const ap = new APlayer({
       {
         name: "你好陌生人",
         artist: '',
-        url: 'https://uploader.shimo.im/f/w09PQZEFVsksW5Ja.mp3',
+        url: 'http://youwe-oss-bucket.oss-cn-shenzhen.aliyuncs.com/xst_dev_android/-1564644582757.mp3',
         cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
       },
       {
         name: '最熟悉的陌生人',
         artist: '',
-        url: 'https://uploader.shimo.im/f/jyIMyNOkyKcL3PII.mp3',
+        url: 'http://youwe-oss-bucket.oss-cn-shenzhen.aliyuncs.com/xst_dev_android/-1564644797055.mp3',
         cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
       },
       {
         name: '十年',
         artist: '',
-        url: 'https://uploader.shimo.im/f/rTg6EOdpRPw7rHKp.mp3',
+        url: 'http://youwe-oss-bucket.oss-cn-shenzhen.aliyuncs.com/xst_dev_android/-1564644813711.mp3',
         cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
       },
       {
         name: '再回首',
         artist: '',
-        url: 'https://uploader.shimo.im/f/77qpMJRv6zQPbJWC.mp3',
+        url: 'http://youwe-oss-bucket.oss-cn-shenzhen.aliyuncs.com/xst_dev_android/-1564644591410.mp3',
         cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
       },
       {
         name: '一无所有',
         artist: '',
-        url: 'https://uploader.shimo.im/f/l8F9G0DKBC8XUIC6.mp3',
+        url: 'http://youwe-oss-bucket.oss-cn-shenzhen.aliyuncs.com/xst_dev_android/-1564644757436.mp3',
         cover: 'https://uploader.shimo.im/f/H6CI761omn89HMI9.jpg',
       }
     ]
